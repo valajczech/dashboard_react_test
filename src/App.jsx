3fd70cea9038c368d5955abe98ef51f08924eaf8
valajczech <jiri.vala11@gmail.com>
2021-09-24 19:58:55 +0200

    Summarywidget design details

--- src/App.jsx
+++ src/App.jsx
@@ -11,10 +11,25 @@ function App() {
       <Sidebar></Sidebar>
       <div className="content">
         <div className="summaries">
-          <SummaryWidget name="Nových uživatelů" number="120" />
-          <SummaryWidget name="Zobrazení" number="400" />
-          <SummaryWidget name="Prokliků" number="355" />
-          <SummaryWidget name="Sdílení" number="69" />
+          <SummaryWidget
+            name="Nových uživatelů"
+            number="120"
+            status="up"
+            percentile="10"
+          />
+          <SummaryWidget
+            name="Zobrazení"
+            number="400"
+            status="up"
+            percentile="25"
+          />
+          <SummaryWidget
+            name="Prokliků"
+            number="355"
+            status="down"
+            percentile="-4"
+          />
+          <SummaryWidget name="Sdílení" number="69" status="down" percentile="-8"/>
         </div>
       </div>
     </div>
--- src/components/SummaryWidget.jsx
+++ src/components/SummaryWidget.jsx
@@ -3,12 +3,31 @@ import "../styles/components/summarywidget.css";
 import { FaArrowDown, FaArrowUp } from "react-icons/fa";
 
 class SummaryWidget extends React.Component {
-  
   render() {
+    let state;
+    if (this.props.state === "down") {
+      this.
+      state = (
+        <div className="status">
+          <FaArrowDown />
+          <p>{this.props.percentile} %</p>
+        </div>
+      );
+    } else {
+      state = (
+        <div className="status">
+          <FaArrowUp />
+          <p>{this.props.percentile} %</p>
+        </div>
+      );
+    }
     return (
       <div className="summary-widget">
-        <h2>{this.props.number}</h2>
-        <p>{this.props.name}</p>
+        <div>
+          <h2>{this.props.number}</h2>
+          <p>{this.props.name}</p>
+        </div>
+        {state}
       </div>
     );
   }
