4cee88e71b886ec64ada1a94e80914943f31e41d
valajczech <jiri.vala11@gmail.com>
2021-09-29 23:43:18 +0200

    Respo

--- src/App.jsx
+++ src/App.jsx
@@ -12,9 +12,56 @@ import { React } from "react";
 function App() {
   return (
     <div className="App">
-      <Topbar></Topbar>
+      <Sidebar />
+      <div className="content">
+        <Topbar />
+        <div className="data">
+          <div className="summaries">
+            <SummaryWidget
+              name="Nových uživatelů"
+              number="120"
+              status="up"
+              percentile="10"
+            />
+            <SummaryWidget
+              name="Zobrazení"
+              number="400"
+              status="up"
+              percentile="25"
+            />
+            <SummaryWidget
+              name="Prokliků"
+              number="355"
+              status="down"
+              percentile="-4"
+            />
+            <SummaryWidget
+              name="Sdílení"
+              number="69"
+              status="down"
+              percentile="-8"
+            />
+          </div>
+          <div className="statistics">
+            <div className="graphs">
+              <VisitorChart />
+              <DeviceChart />
+            </div>
+            <RecentLogsTable />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default App;
+
+/*
+
+<div className="App">
       <Sidebar></Sidebar>
       <div className="content">
+      <Topbar></Topbar>
         <div className="summaries">
           <SummaryWidget
             name="Nových uživatelů"
@@ -50,6 +97,4 @@ function App() {
         </div>
       </div>
     </div>
-  );
-}
-export default App;
+*/
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -29,94 +29,96 @@ class Sidebar extends React.Component {
   render() {
     return (
       <div className={this.state.isMinimized ? "sidebar minimized" : "sidebar"}>
-        <div className="brand">
-          <h3>{this.state.isMinimized ? "DA" : "DCSoft - Admin"}</h3>
-        </div>
-        <div className="content">
-          <div className="route" id="dashboard-route">
-            <div>
-              <FaThLarge className="icon" />
-              <p>Nástěnka</p>
+        <div>
+          <div className="brand">
+            <h3>{this.state.isMinimized ? "DA" : "DCSoft - Admin"}</h3>
+          </div>
+          <div className="bar-content">
+            <div className="route" id="dashboard-route">
+              <div>
+                <FaThLarge className="icon" />
+                <p>Nástěnka</p>
+              </div>
+            </div>
+            <div className="wrapper" id="modules">
+              <h3>Moduly</h3>
+              <div className="routes">
+                <a href="#" className="route">
+                  <div>
+                    <FaBookmark className="icon" />
+                    <p>Položky</p>
+                  </div>
+                  <FaChevronLeft className="arrow" />
+                </a>
+                <a href="#" className="route">
+                  <div>
+                    <FaEdit className="icon" />
+                    <p>Příspěvky</p>
+                  </div>
+                  <FaChevronLeft className="arrow" />
+                </a>
+                <a href="#" className="route">
+                  <div>
+                    <FaTable className="icon" />
+                    <p>Tabulky</p>
+                  </div>
+                  <FaChevronLeft className="arrow" />
+                </a>
+                <a href="#" className="route">
+                  <div>
+                    <FaChartBar className="icon" />
+                    <p>Grafy</p>
+                  </div>
+                  <FaChevronLeft className="arrow" />
+                </a>
+                <a href="#" className="route">
+                  <div>
+                    <FaMap className="icon" />
+                    <p>Mapy</p>
+                  </div>
+                  <FaChevronLeft className="arrow" />
+                </a>
+              </div>
+            </div>
+            <div className="wrapper" id="sections">
+              <h3>Sekce</h3>
+              <div className="routes">
+                <a href="#" className="route">
+                  <div>
+                    <FaEnvelope className="icon" />
+                    <p>Pošta</p>
+                  </div>
+                  <FaChevronLeft className="arrow" />
+                </a>
+                <a href="#" className="route">
+                  <div>
+                    <FaCalendarAlt className="icon" />
+                    <p>Kalendář</p>
+                  </div>
+                  <FaChevronLeft className="arrow" />
+                </a>
+                <a href="#" className="route">
+                  <div>
+                    <FaFileAlt className="icon" />
+                    <p>Stránky</p>
+                  </div>
+                  <FaChevronLeft className="arrow" />
+                </a>
+                <a href="#" className="route">
+                  <div>
+                    <FaSitemap className="icon" />
+                    <p>Struktura</p>
+                  </div>
+                  <FaChevronLeft className="arrow" />
+                </a>
+              </div>
             </div>
           </div>
-          <div className="wrapper" id="modules">
-            <h3>Moduly</h3>
-            <div className="routes">
-              <a href="#" className="route">
-                <div>
-                  <FaBookmark className="icon" />
-                  <p>Položky</p>
-                </div>
-                <FaChevronLeft className="arrow" />
-              </a>
-              <a href="#" className="route">
-                <div>
-                  <FaEdit className="icon" />
-                  <p>Příspěvky</p>
-                </div>
-                <FaChevronLeft className="arrow" />
-              </a>
-              <a href="#" className="route">
-                <div>
-                  <FaTable className="icon" />
-                  <p>Tabulky</p>
-                </div>
-                <FaChevronLeft className="arrow" />
-              </a>
-              <a href="#" className="route">
-                <div>
-                  <FaChartBar className="icon" />
-                  <p>Grafy</p>
-                </div>
-                <FaChevronLeft className="arrow" />
-              </a>
-              <a href="#" className="route">
-                <div>
-                  <FaMap className="icon" />
-                  <p>Mapy</p>
-                </div>
-                <FaChevronLeft className="arrow" />
-              </a>
-            </div>
+          <div className="minimize">
+            <button onClick={this.handleMinimize}>
+              {this.state.isMinimized ? <FaArrowRight /> : <FaArrowLeft />}
+            </button>
           </div>
-          <div className="wrapper" id="sections">
-            <h3>Sekce</h3>
-            <div className="routes">
-              <a href="#" className="route">
-                <div>
-                  <FaEnvelope className="icon" />
-                  <p>Pošta</p>
-                </div>
-                <FaChevronLeft className="arrow" />
-              </a>
-              <a href="#" className="route">
-                <div>
-                  <FaCalendarAlt className="icon" />
-                  <p>Kalendář</p>
-                </div>
-                <FaChevronLeft className="arrow" />
-              </a>
-              <a href="#" className="route">
-                <div>
-                  <FaFileAlt className="icon" />
-                  <p>Stránky</p>
-                </div>
-                <FaChevronLeft className="arrow" />
-              </a>
-              <a href="#" className="route">
-                <div>
-                  <FaSitemap className="icon" />
-                  <p>Struktura</p>
-                </div>
-                <FaChevronLeft className="arrow" />
-              </a>
-            </div>
-          </div>
-        </div>
-        <div className="minimize">
-          <button onClick={this.handleMinimize}>
-            {this.state.isMinimized ? <FaArrowRight /> : <FaArrowLeft />}
-          </button>
         </div>
       </div>
     );
